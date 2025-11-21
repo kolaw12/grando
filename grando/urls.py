99165453ff28c52da1daab5cdfa446dfa8367e88
opeoluwa <kolawolemoses50@gmail.com>
2025-11-22 00:12:23 +0100

    Final working requirements with psycop2-binary 2.9.9

--- grando/urls.py
+++ grando/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 # from django.views.generic import TemplateView
 # from django.http import HttpResponse
 
@@ -31,6 +31,6 @@ urlpatterns = [
     # path('', home),
 ]
 
-if not settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if not settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
