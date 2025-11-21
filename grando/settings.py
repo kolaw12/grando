5dbaa6b7e7bec87d19f74a469bb4e104ef7ea4df
opeoluwa <kolawolemoses50@gmail.com>
2025-11-22 00:59:19 +0100

    Final working requirements with psycop2-binary 2.9.9

--- grando/settings.py
+++ grando/settings.py
@@ -14,12 +14,10 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# STATIC_DIR = os.path.join(BASE_DIR, 'static')
-# MEDIA_ROOT = BASE_DIR / 'media'
-# MEDIA_URL = '/media/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+ 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+ 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -124,16 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-# STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATICFILES_DIRS = [BASE_DIR/'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+ 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
-# Default primary key field type
+STATICFILES_DIRS=[BASE_DIR/ 'static']
+STATIC_ROOT=BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+# t primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
