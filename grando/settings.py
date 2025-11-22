206a50d207ee3ee4e2d2d3ae5b43c43c2eeed73d
opeoluwa <kolawolemoses50@gmail.com>
2025-11-22 02:30:41 +0100

    Final working requirements with psycop2-binary 2.9.9

--- grando/settings.py
+++ grando/settings.py
@@ -67,11 +67,11 @@ ROOT_URLCONF = 'grando.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ ],
+        'DIRS': [],  # or ['templates']
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug'
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -79,6 +79,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'grando.wsgi.application'
 
