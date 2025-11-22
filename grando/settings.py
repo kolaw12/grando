341e5418e193ac2fb535f9e6672e7339386e79e4
opeoluwa <kolawolemoses50@gmail.com>
2025-11-22 01:54:51 +0100

    Final working requirements with psycop2-binary 2.9.9

--- grando/settings.py
+++ grando/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
  
@@ -79,14 +80,11 @@ WSGI_APPLICATION = 'grando.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('PGDATABASE'),
-        'USER': os.getenv('PGUSER'),
-        'PASSWORD': os.getenv('PGPASSWORD'),
-        'HOST': os.getenv('PGHOST'),
-        'PORT':os.getenv('PGPORT'),
-    }
+    'default': dj_database_url(
+        default = os.getenv('DATABASE_URL'),
+        conn_max_age = 600,
+        ssl_require = True,
+    )
 }
 
 
