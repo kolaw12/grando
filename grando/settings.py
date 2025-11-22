ad8bb235c2290d249c33a1dbd96a546fca86a8fd
opeoluwa <kolawolemoses50@gmail.com>
2025-11-22 10:33:52 +0100

    Fixed Bookings

--- grando/settings.py
+++ grando/settings.py
@@ -33,12 +33,22 @@ CSRF_TRUSTED_ORIGINS = [
     "https://web-production-2298a.up.railway.app",
     "https://*.railway.app",
 ]
-EMAIL_USE_TLS = True
+ 
+ 
+ 
+ 
+ 
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'crouchp085@gmail.com'
-EMAIL_HOST_PASSWORD = 'ndyqsvksdvfxixam'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv("BREVO_SMTP_USER")
+EMAIL_HOST_PASSWORD = os.getenv("BREVO_SMTP_KEY")
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
 # ---------------------------------------------------------
 # INSTALLED APPS
 # ---------------------------------------------------------
